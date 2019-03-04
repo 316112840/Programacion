64df09781e061e3c29e4399e4b835da9480176c8
316112840 <mariana_yasmin@ciencias.unam.mx>
2019-03-04 13:38:29 -0600

    Estos son dos archivos, uno que contiene la clase Producto y el otro es para probar que la clase funciona

---
+++ Pruebas.py
@@ -0,0 +1,10 @@
+from Producto import *
+S = "====================================================="
+Producto1 = Producto("Licuadora", "Oster", "Electrodoméstcos", 1600.0 )
+Producto1.MostrarDetalles()
+print(S)
+Producto2 = Producto("Whiskas,delicias rellenas", "Whiskas","Mascotas", 54.50)
+Producto2.MostrarDetalles()
+print(S)
+Producto3 = Producto("Aceite de coco", "San Lucas", "Despensa", 69.0)
+Producto3.MostrarDetalles()
