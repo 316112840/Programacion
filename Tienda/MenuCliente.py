5ad0c151f2e62c4c23104f6562a63c4b619599f8
316112840 <mariana_yasmin@ciencias.unam.mx>
2019-03-25 12:17:10 -0600

    Esta es una clase que contiene un método que nos permite registrar un cliente

---
+++ Tienda/MenuCliente.py
@@ -0,0 +1,12 @@
+# Esta es una clase que tiene como método el registro de un cliente, los datos que se ingresarán son el nombre y su saldo.
+# Martínez García Mariana Yasmin
+
+
+class MenuCliente:
+
+    def __init__(self):
+        pass
+
+    def NuevoCliente(self):
+        nombre = input("Ingrese su nombre: ")
+        saldo = input("Ingrese su saldo: ")
